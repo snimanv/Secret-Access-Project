73809a4b8f625858d161066fc9259542732c60cc
snimanv <snimanv@gmail.com>
2024-01-17 12:32:45 +0530

    Almost done...just need to debug the code...

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 8000;
+var password = '';
+var userAuthenticate = false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function accessCheck(req, res, next) {
+    password = req.body['password'];
+    if (password == '^Tech123!') {
+        userAuthenticate = true;
+    };
+    next();
+};
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+    app.use(accessCheck);
+    res.sendFile(__dirname + "/public/access.html");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
